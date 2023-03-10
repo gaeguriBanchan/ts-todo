1e5cc129da187cc037ffd363e2ca3fd0001ab27a
gaeguriBanchan <rlawowls93@gamil.com>
2023-03-10 11:23:16 +0900

    파이어베이스

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import * as css from "./style/style";
+import { FormOutlined } from "@ant-design/icons";
+import { CallBacksType, StatesType } from "./AppContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Todo from "./pages/Todo";
+import NotFound from "./pages/NotFound";
+import TodoEdit from "./pages/TodoEdit";
+type propsType = {
+  states: StatesType;
+  callBacks: CallBacksType;
+};
+
+function App({ states, callBacks }: propsType) {
+  return (
+    <BrowserRouter>
+      <css.Wrapper className="wrap">
+        <css.Inner className="inner">
+          <css.AppTitle>
+            <FormOutlined />
+            TodoList App
+          </css.AppTitle>
+        </css.Inner>
+        {/* 라우팅 영역 */}
+        <Routes>
+          {/* 첫화면 : 입력창, 목록창 */}
+          <Route
+            path="/"
+            element={<Todo states={states} callBacks={callBacks} />}
+          />
+          {/* 수정화면 : 편집창 */}
+          <Route
+            path="/edit/:uid"
+            element={<TodoEdit states={states} callBacks={callBacks} />}
+          />
+          {/* 주소오류 */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </css.Wrapper>
+    </BrowserRouter>
+  );
+}
+
+export default App;
